05cb25030d4927c4ef981e43fa579d8206173443
mmmmay <premikamay@gmail.com>
2023-06-10 21:28:54 +0700

    Create Constant.dart

---
+++ lib/constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kTextField = Color(0xFFE0F4FF);
+
+const kButtonColorBlue = Color(0xFF4D8CFE);
+const kButtonColorRed = Color(0xFFFF0000);
+
+const kFontBlue = Color(0xFF4D8CFE);
+const kFontBlack = Color(0xFF000000);
+const kFontGrey1 = Color(0xFFB8B1B1);
+const kFontGrey2 = Color(0xFF878484);
+const kFontGrey3 = Color(0xFF767676);
