8e0d1ca5a312361887faae7cd856629d6a1e09aa
icesaran3 <icesaran3@gmail.com>
2023-06-22 13:19:36 +0700

    [ADD] can create new post

--- lib/screen/pages/gMap.dart
+++ lib/screen/pages/gMap.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -21,21 +26,29 @@ class _MapScreenState extends State<MapScreen> {
   Set<Marker> _markers = {};
   Set<Marker> _selectedMarkers = Set<Marker>();
   Set<Marker> _addMarkers = {};
+  late SharedPreferences prefs;
+  var myToken;
 
   @override
   void initState() {
+    initSharedPref();
     super.initState();
     _location.onLocationChanged.listen((LocationData currentLocation) {
       setState(() {
         _currentLocation = currentLocation;
       });
     });
-
+    FirebaseStorage storage = FirebaseStorage.instance;
     getMarkerData();
-
     Timer.periodic(const Duration(seconds: 30), (_) {
       getMarkerData();
     });
+  }
+
+  void initSharedPref() async {
+    prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+    myToken = token;
   }
 
   void getMarkerData() async {
@@ -99,6 +112,7 @@ class _MapScreenState extends State<MapScreen> {
               }
             },
           ),
+          // icon: markerbitmap,
         );
 
         newMarkers.add(marker);
@@ -159,47 +173,121 @@ class _MapScreenState extends State<MapScreen> {
     }
   }
 
-  void _addMarker(
-      String locationName, double latitude, double longitude) async {
-    try {
-      final url = 'http://10.0.2.2:3000/api/pins';
+  UploadTask? uploadTask;
+  PlatformFile? pickedFile;
 
-      final response = await http.post(
-        Uri.parse(url), // Replace with your backend URL
+  void _getImageFromGallery() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+    setState(() {
+      pickedFile = result.files.first;
+    });
+    _buildImageWidget();
+  }
+
+  final pinId = 'a126f427-608f-4c6a-ac5a-f5198396f645';
+  void _addMarkerWithPost(String locationName, double latitude,
+      double longitude, PlatformFile? pickedFile, String postDetail) async {
+    try {
+      final pinsUrl = 'http://10.0.2.2:3000/api/pins';
+      final postsUrl = 'http://10.0.2.2:3000/api/posts';
+
+      // Step 1: Add a new pin
+      final pinResponse = await http.post(
+        Uri.parse(pinsUrl),
+        headers: {'authorization': 'Bearer $myToken'},
         body: {
-          'user_id':
-              'f5ac0db7-78da-439a-86a0-4dc1d810abe2', // Replace with the appropriate user ID
           'location_name': locationName,
           'latitude': latitude.toString(),
           'longitude': longitude.toString(),
         },
       );
 
-      if (response.statusCode == 200) {
-        // Marker added successfully
-        print('Marker added successfully');
-        // Update the markers list with the new marker
-        setState(() {
-          final marker = Marker(
-            markerId: MarkerId(DateTime.now().toString()),
-            position: LatLng(latitude, longitude),
-            infoWindow: InfoWindow(
-              title: locationName,
-            ),
+      if (pinResponse.statusCode == 200) {
+        // Pin added successfully
+
+        // Extract the pin ID from the pin response
+        final responseData = jsonDecode(pinResponse.body);
+
+        // Step 2: Upload the file to Firebase Storage
+        if (pickedFile != null) {
+          FirebaseStorage storage = FirebaseStorage.instance;
+          Reference storageRef = storage
+              .ref()
+              .child('images/${DateTime.now().millisecondsSinceEpoch}');
+
+          final metadata = SettableMetadata(
+            contentType: 'image/jpeg',
           );
-          _markers.add(marker);
-        });
+
+          uploadTask = storageRef.putFile(
+            File(pickedFile.path!),
+            metadata,
+          );
+
+          final TaskSnapshot snapshot = await uploadTask!.whenComplete(() {});
+          final downloadURL = await snapshot.ref.getDownloadURL();
+
+          // Step 3: Create a new post with the download URL
+          final postResponse = await http.post(
+            Uri.parse(postsUrl),
+            headers: {'authorization': 'Bearer $myToken'},
+            body: {
+              'pin_id': pinId,
+              'post_detail': postDetail,
+              'post_image': downloadURL,
+            },
+          );
+
+          if (postResponse.statusCode == 200) {
+            // Post created successfully
+            print('Post created successfully');
+            // Update the markers list with the new marker
+            setState(() {
+              final marker = Marker(
+                markerId: MarkerId(DateTime.now().toString()),
+                position: LatLng(latitude, longitude),
+                infoWindow: InfoWindow(
+                  title: locationName,
+                ),
+              );
+              _markers.add(marker);
+            });
+          } else {
+            // Error creating post
+            print('Error creating post');
+          }
+
+          if (postResponse.statusCode == 200) {
+            // Post created successfully
+            print('Post created successfully');
+            // Update the markers list with the new marker
+            setState(() {
+              final marker = Marker(
+                markerId: MarkerId(DateTime.now().toString()),
+                position: LatLng(latitude, longitude),
+                infoWindow: InfoWindow(
+                  title: locationName,
+                ),
+              );
+              _markers.add(marker);
+            });
+          } else {
+            // Error creating post
+            print('Error creating post');
+          }
+        }
       } else {
-        // Error adding marker
-        print('Error adding marker');
+        // Error adding pin
+        print('Error adding pin');
       }
-    } catch (e) {
-      // Exception occurred
-      print('Exception occurred: $e');
+    } catch (error) {
+      print('Error: $error');
     }
   }
 
   TextEditingController locationController = TextEditingController();
+  TextEditingController postDetailController = TextEditingController();
 
   void _showMarkerPopup(Marker marker) {
     showDialog(
@@ -321,6 +409,23 @@ class _MapScreenState extends State<MapScreen> {
     );
   }
 
+  Widget _buildImageWidget() {
+    if (pickedFile == null) {
+      return Image.asset(
+        'assets/images/upload pic.png',
+        width: 100.0,
+        height: 100.0,
+      );
+    } else {
+      final file = File(pickedFile!.path!);
+      return Image.memory(
+        file.readAsBytesSync(),
+        width: 100.0,
+        height: 100.0,
+      );
+    }
+  }
+
   void _showNextPopup(Marker marker) {
     showDialog(
       barrierDismissible: false,
@@ -379,16 +484,13 @@ class _MapScreenState extends State<MapScreen> {
                       ),
                       padding: const EdgeInsets.fromLTRB(70, 25, 70, 25),
                       child: GestureDetector(
-                        onTap: () {},
+                        onTap: () {
+                          _getImageFromGallery();
+                        },
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            // const SizedBox(height: 10.0),
-                            Image.asset(
-                              'assets/images/upload pic.png',
-                              width: 100.0,
-                              height: 100.0,
-                            ),
+                            _buildImageWidget(),
                           ],
                         ),
                       ),
@@ -420,6 +522,7 @@ class _MapScreenState extends State<MapScreen> {
                           const SizedBox(height: 10.0),
                           Expanded(
                             child: TextFormField(
+                              controller: postDetailController,
                               decoration: const InputDecoration.collapsed(
                                 hintText: 'Tell us about this location..',
                                 hintStyle: TextStyle(fontSize: 12.0),
@@ -443,7 +546,9 @@ class _MapScreenState extends State<MapScreen> {
                               String locationName = locationController.text;
                               double latitude = marker.position.latitude;
                               double longitude = marker.position.longitude;
-                              _addMarker(locationName, latitude, longitude);
+                              String postDetail = postDetailController.text;
+                              _addMarkerWithPost(locationName, latitude,
+                                  longitude, pickedFile, postDetail);
                               Navigator.pop(context);
                               _cancelEditingMode();
                             },
@@ -480,110 +585,13 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 
-  // void _deleteMarker(MarkerId markerId) {
-  //   if (_editingMode) {
-  //     showDialog(()
-  //       context: context,
-  //       builder: (context) {
-  //         return AlertDialog(
-  //           shape: RoundedRectangleBorder(
-  //             borderRadius: BorderRadius.circular(15.0),
-  //           ),
-  //           backgroundColor: const Color.fromARGB(255, 224, 244, 255),
-  //           content: Container(
-  //             width: 383.0, // Set the desired width
-  //             height: 90.0, // Set the desired height
-  //             child: Column(
-  //               mainAxisSize: MainAxisSize.min,
-  //               mainAxisAlignment: MainAxisAlignment.center,
-  //               children: [
-  //                 const Padding(
-  //                   padding: EdgeInsets.only(
-  //                       bottom: 10.0), // Add padding to the bottom
-  //                   child: Text(
-  //                     'Confirm to remove the marker ?',
-  //                     textAlign: TextAlign.center,
-  //                     style: TextStyle(
-  //                       fontSize: 18.0, // Set the desired font size
-  //                     ),
-  //                   ),
-  //                 ),
-  //                 Row(
-  //                   mainAxisAlignment: MainAxisAlignment.center,
-  //                   children: [
-  //                     ElevatedButton(
-  //                       onPressed: () {
-  //                         setState(() {
-  //                           _markers.removeWhere(
-  //                               (marker) => marker.markerId == markerId);
-  //                         });
-  //                         Navigator.of(context).pop(); // Close the dialog
-  //                       },
-  //                       child: const Text(
-  //                         'Yes',
-  //                         style: TextStyle(
-  //                             fontSize: 13.0), // Set the desired font size
-  //                       ),
-  //                       style: ButtonStyle(
-  //                         padding:
-  //                             MaterialStateProperty.all<EdgeInsetsGeometry>(
-  //                           EdgeInsets.symmetric(
-  //                               vertical: 12.0,
-  //                               horizontal: 45.0), // Adjust the padding
-  //                         ),
-  //                         backgroundColor:
-  //                             MaterialStateProperty.all<Color>(Colors.blue),
-  //                         shape:
-  //                             MaterialStateProperty.all<RoundedRectangleBorder>(
-  //                           RoundedRectangleBorder(
-  //                             borderRadius: BorderRadius.circular(50.0),
-  //                           ),
-  //                         ),
-  //                       ),
-  //                     ),
-  //                     const SizedBox(width: 30.0),
-  //                     ElevatedButton(
-  //                       onPressed: () {
-  //                         Navigator.of(context).pop(); // Close the dialog
-  //                       },
-  //                       child: const Text(
-  //                         'No',
-  //                         style: TextStyle(
-  //                             fontSize: 13.0), // Set the desired font size
-  //                       ),
-  //                       style: ButtonStyle(
-  //                         padding:
-  //                             MaterialStateProperty.all<EdgeInsetsGeometry>(
-  //                           EdgeInsets.symmetric(
-  //                               vertical: 12.0,
-  //                               horizontal: 50.0), // Adjust the padding
-  //                         ),
-  //                         backgroundColor:
-  //                             MaterialStateProperty.all<Color>(Colors.red),
-  //                         shape:
-  //                             MaterialStateProperty.all<RoundedRectangleBorder>(
-  //                           RoundedRectangleBorder(
-  //                             borderRadius: BorderRadius.circular(45.0),
-  //                           ),
-  //                         ),
-  //                       ),
-  //                     ),
-  //                   ],
-  //                 ),
-  //               ],
-  //             ),
-  //           ),
-  //         );
-  //       },
-  //     );
-  //   }
-  // }
-
   void _cancelEditingMode() {
     setState(() {
       _editingMode = false;
       _addMarkers.clear();
       locationController.clear();
+      postDetailController.clear();
+      pickedFile = null;
     });
     getMarkerData();
   }
